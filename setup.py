d71c86cf66e256c12cc0017ac22ac29d1a4efeb5
Israel9974 <Israelcornejozapata@gmail.com>
2025-08-25 10:36:02 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ setup(
         'matplotlib>=3.0.0',
         'seaborn>=0.10.0',
         'scipy>=1.4.0',
-        'statsmodels>=0.11.0'
+        'statsmodels>=0.11.0',
+        'Pillow>=9.2.0'
     ],  # Aquí puedes poner dependencias si las hay
     author='Israel Cornejo',
     description='Genera un reporte en PDF para Análisis Exploratorio de Variables int y float',
@@ -22,5 +23,6 @@ setup(
     keywords='EDA, PDF, análisis exploratorio, pandas, numpy, matplotlib, seaborn, scipy, statsmodels',
     include_package_data=True,
 )
+
 
 
